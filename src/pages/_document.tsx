943c7d00001b2f8ca6e3c3e9a76a05a76e6b425e
Timo Strackfeldt <timo@strackfeldt.com>
2022-06-23 16:33:21 +0200

    link manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta name="theme-color" content="#1e293b" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body className="bg-gray-200">
         <Main />
         <NextScript />
